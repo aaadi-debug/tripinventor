43a08221528e5ddc84c700caed6e9528f4c5a190
Aditya Raj Gupta <adityaraj51202@gmail.com>
2025-02-04 04:56:43 +0530

    aditya pc

--- backend/app.js
+++ backend/app.js
@@ -56,7 +56,7 @@ app.use("/api/home", homeRoutes);
 
 
 // Start the server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
